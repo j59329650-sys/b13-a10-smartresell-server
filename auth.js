ff8b2a437d9b55722c09718cc4cdc7b7a4879015
tahmina tanju <you@example.com>
2026-06-23 03:41:54 +0600

    fix: auth config and missing user put route

--- auth.js
+++ auth.js
@@ -7,16 +7,26 @@ dotenv.config();
 
 export const auth = betterAuth({
     database: mongodbAdapter(db),
+    
+    
+    emailAndPassword: {
+        enabled: true,
+        autoSignInToProvider: true
+    },
+
+    
     socialProviders: {
         google: {
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         },
     },
+
+    
     session: {
+        strategy: "jwt",
         cookieCache: {
-            enabled: true,
-            strategy: "jwt"
+            enabled: true
         }
     }
 });
